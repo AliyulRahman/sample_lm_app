d0b2220d7f68dd8fea058196e9e8632496194576
AliyulRahman <65935555+AliyulRahman@users.noreply.github.com>
2025-05-06 18:01:32 +0300

    Contact us added in route

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import LoginForm from "./components/Login/LoginForm";
 import Admin from "./components/Admin/";
 import "./App.css";
 import Services from "./components/Services";
-import Navbar from "./components/Navbar/";
+import Navbar from "./components/Navbar";
+import ContactUs from "./components/ContactUs"
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
         <Route path="/login" element={<LoginForm />} />
         <Route path="/admin" element={<Admin />} />
         <Route path="/services" element={<Services />} />
+        <Route path="/contactus" element={<ContactUs />} />
       </Routes>
     </Router>
   );
